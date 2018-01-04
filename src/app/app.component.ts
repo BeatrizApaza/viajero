c6c677b5ce28cfa4c3d16b78d9a1715a80878328
BeatrizApaza <bky.apaza@gmail.com>
2018-01-04 09:47:20 -0400

    Modificacion del titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lideres Viajando en el Ministerio';
+  title = 'Modificado 1 Lideres Viajando en el Ministerio';
 }
